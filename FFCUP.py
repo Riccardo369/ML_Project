2c03678e76da0c39f3ba6ab28278c8b6fb9dddcb
Riccardo369 <riccard.marcaccio@studenti.unicam.it>
2024-02-01 07:12:17 +0100

    .

--- FFCUP.py
+++ FFCUP.py
@@ -9,18 +9,42 @@ from loss_functions import mee_loss,mse_loss
 from parameter_grid import ParameterGrid
 from utils import evaluate_performance, k_fold, monk_classification
 
+import math
+
 #loading monk dataset
 cup_tr=TakeCupDataset("FilesData/ML-CUP23-TR.csv")
 dataset=DataSet(cup_tr,10,3)
 cup_vl=TakeCupDataset("FilesData/ML-CUP23-TS.csv")
 vl_dataset=DataSet(cup_tr,10,3)
 
+a1 = 0.03
+a2 = 0.03
+a3 = 0.03
+a4 = 0.03
+a5 = 0.03
+
 nn=FFNeuralNetwork(
     InputLayer(10,np.eye(10),lambda x:x,lambda x:1),
     [
-        Layer(10,np.random.rand(10,10)*0.5),
-        Layer(10,np.random.rand(10,10)*0.5),
-        Layer(10,np.random.rand(10,10)*0.5),
+        #Layer(10,np.random.rand(10,10)*0.5),
+        #Layer(10,np.random.rand(10,10)*0.5, lambda x: x if x > 0 else a1*x, lambda x: 1 if x > 0 else a1),
+        Layer(10,np.random.rand(10,10)*0.5, lambda x: x if x > 0 else a1*((math.e**x)-1), lambda x: 1 if x > 0 else a1*(math.e**x)),
+        
+        #Layer(10,np.random.rand(10,10)*0.5),
+        #Layer(10,np.random.rand(10,10)*0.5, lambda x: x if x > 0 else a2*x, lambda x: 1 if x > 0 else a2),
+        Layer(9,np.random.rand(9,10)*0.5, lambda x: x if x > 0 else a2*((math.e**x)-1), lambda x: 1 if x > 0 else a2*(math.e**x)),
+
+        #Layer(10,np.random.rand(10,10)*0.5),
+        #Layer(10,np.random.rand(10,10)*0.5, lambda x: x if x > 0 else a3*x, lambda x: 1 if x > 0 else a3),
+        Layer(10,np.random.rand(10,9)*0.5, lambda x: x if x > 0 else a3*((math.e**x)-1), lambda x: 1 if x > 0 else a3*(math.e**x)),
+
+        #Layer(10,np.random.rand(10,10)*0.5),
+        #Layer(10,np.random.rand(10,10)*0.5, lambda x: x if x > 0 else a4*x, lambda x: 1 if x > 0 else a4),
+        Layer(10,np.random.rand(10,10)*0.5, lambda x: x if x > 0 else a4*((math.e**x)-1), lambda x: 1 if x > 0 else a4*(math.e**x)),
+
+        #Layer(10,np.random.rand(10,10)*0.5),
+        #Layer(10,np.random.rand(10,10)*0.5, lambda x: x if x > 0 else a5*x, lambda x: 1 if x > 0 else a5),
+        #Layer(10,np.random.rand(10,10)*0.5, lambda x: x if x > 0 else a5*((math.e**x)-1), lambda x: 1 if x > 0 else a5*(math.e**x)),
     ],
     OutputLayer(3,np.random.rand(3,10),lambda x:x,lambda x:1)
 )
@@ -45,16 +69,18 @@ best_model_state=None
 best_model_performance=np.inf
 best_model_index=-1
 
+#Sembra che per ottenere un grafico sensato:
+
 grid=ParameterGrid({
-    "learning_rate":np.array([0.01,0.001,0.0001]),
-    "weight_decay":np.array([0.0]),
-    "momentum":np.array([0.0,0.1,0.4,0.8]),
-    "batch_size":np.array([STOCHASTIC]),
+    "learning_rate": np.array([0.0001]),                 #Più è complesso il modello, più basso deve essere
+    "weight_decay": np.array([0.0001]),                  #Contro overfitting
+    "momentum": np.array([0.1]),                              
+    "batch_size": np.array([STOCHASTIC]),
+    "error_percent": np.array([1]),
   })
 
-patience=100
-max_epochs=10000
-error_percent=1.1
+patience=1000
+max_epochs=200
 
 for i in range(grid.get_size()):
     print("hyperparameters combination no.",i+1,"=",grid[i])
@@ -74,42 +100,52 @@ for i in range(grid.get_size()):
         plt.title(f"comb no.{i+1}, internal set {folds_number} fold with values "+" ".join([ f"{k}={v}" for k,v in grid[i].items()]))
         epoch=0
         stop_epochs=0
+        error_percent = hyperparameters["error_percent"]
         while (True):
-            nn.fit(tr,
+          nn.fit(tr,
                 learning_rate=hyperparameters["learning_rate"],
                 weight_decay=hyperparameters["weight_decay"],
                 momentum=hyperparameters["momentum"],
-                batch_size= len(tr) if hyperparameters["batch_size"] < 0 
+                batch_size= len(tr) if hyperparameters["batch_size"] < 0
                                     else hyperparameters["batch_size"])
-            tr_mse,tr_mee=evaluate_performance(nn,DataSet(tr,10,3),mse_loss,mee_loss)
-            vl_mse,vl_mee=evaluate_performance(nn,DataSet(vl,10,3),mse_loss,mee_loss)
-            training_mse.append(tr_mse)
-            training_mee.append(tr_mee)
-            validation_mse.append(vl_mse)
-            validation_mee.append(vl_mee)
-            plt.plot(training_mee,label="training MEE",color="blue")
-            plt.plot(training_mse,label="training MSE",color="red")
-            plt.plot(validation_mee,label="validation MEE",color="blue")
-            plt.plot(validation_mse,label="training MSE",color="red")
+          tr_mse,tr_mee=evaluate_performance(nn,DataSet(tr,10,3),mse_loss,mee_loss)
+          vl_mse,vl_mee=evaluate_performance(nn,DataSet(vl,10,3),mse_loss,mee_loss)
+          training_mse.append(tr_mse)
+          training_mee.append(tr_mee)
+          validation_mse.append(vl_mse)
+          validation_mee.append(vl_mee)
+
+          x = 9
+
+          if(epoch > x+1):
+
+            if((abs(training_mse[-1] - training_mse[-x-2]) < 1) and 
+               (abs(training_mee[-1] - training_mee[-x-2]) < 1) and 
+               (abs(validation_mse[-1] - validation_mse[-x-2]) < 1) and 
+               (abs(validation_mee[-1] - validation_mee[-x-2]) < 1)):
+              print("Stop criteria")
+              break
             
-            if (old_vl_mee/vl_mee)>error_percent:
-                stop_epochs+=1
-            else:
-                stop_epochs=0
-            if stop_epochs >= patience or epoch >= max_epochs:
-                 break
-            epoch+=1
-                
-                
-        
+          if stop_epochs >= patience or epoch >= max_epochs:
+                break
+          epoch += 1
+
+        plt.plot(training_mee,label="training MEE",color="blue")
+        plt.plot(training_mse,label="training MSE",color="red")
+        plt.plot(validation_mee,label="validation MEE",color="green")
+        plt.plot(validation_mse,label="validation MSE",color="purple")
+        plt.legend()
+
+
         mee_performance+=vl_mee
     mee_performance/=folds_number
     print("final performance",mee_performance)
     if mee_performance < best_model_performance:
         best_model_performance=mee_performance
         best_model_index=i
-    plt.savefig(f"Plot Graphic/CUP/{folds_number}-fold {" ".join([ f"{k}={v}" for k,v in grid[i].items()])}.png")
-    
+    plt.savefig(f"../drive/Shareddrives/ML project/Project/Plot graphic CUP/{folds_number}-fold {' '.join([ f'{k}={v}' for k,v in grid[i].items()])}.png")
+    plt.show()
+
 training_mse=[]
 validation_mse=[]
 validation_mee=[]
@@ -121,7 +157,7 @@ for epoch in range(1000):
                 learning_rate=best_hyperparameters["learning_rate"],
                 weight_decay=best_hyperparameters["weight_decay"],
                 momentum=best_hyperparameters["momentum"],
-                batch_size= dataset.size() if best_hyperparameters["batch_size"] < 0 
+                batch_size= dataset.size() if best_hyperparameters["batch_size"] < 0
                                     else best_hyperparameters["batch_size"])
             tr_mse,tr_mee=evaluate_performance(nn,dataset,mse_loss,mee_loss)
             vl_mse,vl_mee=evaluate_performance(nn,vl_dataset,mse_loss,mee_loss)
@@ -129,7 +165,7 @@ for epoch in range(1000):
             training_mee.append(tr_mee)
             validation_mse.append(vl_mse)
             validation_mee.append(vl_mee)
-"""     
+"""
 plt.clf()
 plt.plot(training_mse,label="training MSE")
 plt.plot(validation_mse,label="test MSE")
--- MONK2FF.py
+++ MONK2FF.py
@@ -21,40 +21,29 @@ monk2_ts=TakeMonksDataSet("FilesData/monks-2.test")
 Data_ts=convert_to_one_hot(["a1","a2","a3","a4","a5","a6"],["class"],monk_encoding,monk2_ts)
 ts_dataset=DataSet(Data_ts,17,1)
 
-#Create model
-nn = FFNeuralNetwork(
-    InputLayer(17,np.eye(17),lambda x:x,lambda x:1),
-    [
-        Layer(5,np.random.rand(5,17)*0.5),
-    ],
-    OutputLayer(1,np.random.rand(1,5),sigmoid, sigmoid_prime)
-)
-
 #Start training
 print(f"beginning training with {dataset.size()} pattern, and {ts_dataset.size()} pattern in the vl set")
 
-initial_state = nn.dump()
-
 STOCHASTIC=1
-BATCH=-1
+BATCH=dataset.size()
 
 best_model_state=None
 best_model_performance=np.inf
 best_model_index=-1
 
-#grid=ParameterGrid({
-    #"learning_rate":np.array([0.3, 0.6]),
-    #"weight_decay":np.array([0.0, 0.002]),
-    #"momentum":np.array([0.0, 0.2]),
-    #"batch_size":np.array([STOCHASTIC]),
-  #})
-
 grid=ParameterGrid({
-    "learning_rate":np.array([0.02]),
-    "weight_decay":np.array([0.00001]),
-    "momentum":np.array([0.9]),
-    "batch_size":np.array([STOCHASTIC]),
+    "learning_rate": np.array([0.1]),
+    "weight_decay": np.array([0]),
+    "momentum": np.array([0.1]*10),
+    "batch_size": np.array([BATCH]),
   })
+
+TitlePlot = "MONK2 batch"
+
+TrainFinalErrorOutputs = []
+ValidationFinalErrorOutputs = []
+TrainFinalAccuracyOutputs = []
+ValidationFinalAccuracyOutputs = []
 
 for i in range(grid.get_size()):
     hyperparameters=grid[i]
@@ -64,15 +53,22 @@ for i in range(grid.get_size()):
     validation_performance_prec=[]
     training_performance_prec=[]
     
-    nn.load(initial_state)
+    #Create new model
+    nn = FFNeuralNetwork(
+        InputLayer(17,np.eye(17),lambda x:x,lambda x:1),
+        [
+            Layer(5,np.random.rand(5,17)*0.5),
+        ],
+        OutputLayer(1,np.random.rand(1,5),sigmoid, sigmoid_prime)
+    )
 
     learning_rate = hyperparameters["learning_rate"]
     weight_decay = hyperparameters["weight_decay"]
     momentum = hyperparameters["momentum"]
-    batch_size = dataset.size() if hyperparameters["batch_size"] < 0 else hyperparameters["batch_size"]
+    batch_size = hyperparameters["batch_size"]
 
     for epoch in range(400):
-        np.random.shuffle(dataset.get_dataset())
+        
         nn.fit(dataset.get_dataset(),
             learning_rate,
             weight_decay,
@@ -83,29 +79,46 @@ for i in range(grid.get_size()):
         vl_loss, vl_prec=evaluate_performance(nn, ts_dataset,mse_loss,monk_classification)
         
         training_performance.append(tr_loss)
-        training_performance_prec.append(tr_prec)
+        training_performance_prec.append(tr_prec*100)
         validation_performance.append(vl_loss)
-        validation_performance_prec.append(vl_prec)
-
+        validation_performance_prec.append(vl_prec*100)
+        
+    TrainFinalErrorOutputs.append(training_performance[-1])
+    ValidationFinalErrorOutputs.append(validation_performance[-1])
+    
+    TrainFinalAccuracyOutputs.append(training_performance_prec[-1])
+    ValidationFinalAccuracyOutputs.append(validation_performance_prec[-1])
+    
     #Show plots
-    plt.plot(training_performance,label="training loss",color="red")
-    plt.plot(validation_performance,label="validation loss",color="blue")
-    plt.title(f'MONK2,{ " ".join([ f"{k}={v} " for k,v in grid[i].items()])}')
+    
+    plt.title(TitlePlot, fontsize=16)
+    plt.plot(training_performance, label="training", color="red")
+    plt.plot(validation_performance, label="validation", color="blue")
+    plt.title(f"{TitlePlot} MSE")
+    plt.xlabel("Epoch")
+    plt.ylabel("Value")
     plt.legend()
     
-    #plt.savefig(f'Plot graphic/MONK2, {" ".join([ f"{k}={v} " for k,v in grid[i].items()])} {i+1} (Loss).png')
-    
-    plt.show()
+    plt.savefig(f'Plot graphic/{TitlePlot} MSE {i+1}.png')
+    #plt.show()
     plt.clf()
+    
+    plt.title(TitlePlot, fontsize=16)
+    plt.plot(training_performance_prec, label="training", color="orange")
+    plt.plot(validation_performance_prec, label="validation", color="purple")
+    plt.title(f"{TitlePlot} Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("%")
+    plt.legend()
+    
+    plt.savefig(f'Plot graphic/{TitlePlot} Accuracy {i+1}.png')
+    #plt.show()
+    plt.clf()
+    
+    print(f'{TitlePlot}, {grid[i]}')
 
-    plt.plot(training_performance_prec,label="training precision",color="orange")
-    plt.plot(validation_performance_prec,label="validation precision",color="blue")
-    plt.title(f'MONK2,{ " ".join([ f"{k}={v} " for k,v in grid[i].items()])}')
-    plt.legend()
-    
-    #plt.savefig(f'Plot graphic/MONK2, {" ".join([ f"{k}={v} " for k,v in grid[i].items()])} {i+1} (Precision).png')
-    
-    plt.show()
-    plt.clf()
-    
-    print(f'MONK2, {grid[i]}')
+print(f"Train loss = Mean: {np.mean(np.array(TrainFinalErrorOutputs))}, Variance = {np.var(np.array(TrainFinalErrorOutputs))}")
+print(f"Validation loss = Mean: {np.mean(np.array(ValidationFinalErrorOutputs))}, Variance = {np.var(np.array(ValidationFinalErrorOutputs))}")
+
+print(f"Train accuracy = Mean: {np.mean(np.array(TrainFinalAccuracyOutputs))}, Variance = {np.var(np.array(TrainFinalAccuracyOutputs))}")
+print(f"Validation accuracy = Mean: {np.mean(np.array(ValidationFinalAccuracyOutputs))}, Variance = {np.var(np.array(ValidationFinalAccuracyOutputs))}")
